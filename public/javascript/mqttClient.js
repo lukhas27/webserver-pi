d1ad6eb0e85cd25ef10fb4b5f85c4f5d18e5d429
Lukas Haspel <lu391has@htwg-konstanz.de>
2021-03-10 17:00:16 +0100

    added colorpicker

--- public/javascript/mqttClient.js
+++ public/javascript/mqttClient.js
@@ -11,13 +11,19 @@ MQTT_PASSWORD = 'Chilldeinleben1';
 const topic= {
   BALKON_LED_STATE: "balkon/leds/state",
   BALKON_LED_COLOR: "balkon/leds/color",
-  BALKON_LED_BRIGHTNESS: "balkon/leds/brightness"
+  BALKON_LED_BRIGHTNESS: "balkon/leds/brightness",
+  BALKON_LED_RANGEMIN: "balkon/leds/borders/min",
+  BALKON_LED_RANGEMAX: "balkon/leds/borders/max"
 };
 
 // topic array for subscribing topics
 const subTopics = 
 [
-  topic.BALKON_LED_STATE
+  topic.BALKON_LED_STATE,
+  topic.BALKON_LED_COLOR,
+  topic.BALKON_LED_BRIGHTNESS,
+  topic.BALKON_LED_RANGEMIN,
+  topic.BALKON_LED_RANGEMAX
 ];
 
 // create a client instance
@@ -32,8 +38,62 @@ client.connect({onSuccess:onConnect, userName:MQTT_USERNAME, password:MQTT_PASSW
 
 
 /*********************************************************
- * MQTT Callback
+ * HTML Configuration
  *********************************************************/
+
+// switch OnOff
+var switchOnOff = document.getElementById("switch-OnOff");
+
+// slider brightness
+var sliderBrightness = document.getElementById("slider-brightness");
+
+// wheel color picker
+var colorPicker = document.getElementById("color-block");
+
+// led borders
+var ledRangeMin = document.getElementById("led-range-min");
+var ledRangeMax = document.getElementById("led-range-max");
+
+initRange1(ledRangeMin);
+initRange2(ledRangeMax);
+
+/*********************************************************
+ * Event Handler
+ *********************************************************/
+
+// switch OnOff
+switchOnOff.onchange = function(){
+  if (switchOnOff.checked){
+    publish(topic.BALKON_LED_STATE, "on");
+  }
+  else {
+    publish(topic.BALKON_LED_STATE, "off");
+  }
+};
+
+// slider brightness
+sliderBrightness.oninput = function(){
+  // publish
+  publish(topic.BALKON_LED_BRIGHTNESS, sliderBrightness.value);
+};
+
+// color picker
+colorPicker.oncolorchange = function(){
+  publish(topic.BALKON_LED_COLOR, colorPicker.value);
+};
+
+// led border
+ledRangeMin.oninput = function(){
+  publish(topic.BALKON_LED_RANGEMIN, ledRangeMin.value);
+};
+ledRangeMax.oninput = function(){
+  publish(topic.BALKON_LED_RANGEMAX, ledRangeMax.value);
+};
+
+/*********************************************************
+ * MQTT Functions
+ *********************************************************/
+
 // called when the client connects
 function onConnect() {
   console.log("MQTT " + MQTT_CLIENTID + " connected!");
@@ -56,50 +116,93 @@ function onMessageArrived(message) {
     case topic.BALKON_LED_STATE:
         ledStateArrived(message.payloadString);
       break;
+    case topic.BALKON_LED_BRIGHTNESS:
+        ledBrightnessArrived(message.payloadString);
+      break;
+    case topic.BALKON_LED_COLOR:
+        ledColorArrived(message.payloadString);
+      break;
   }
   console.log("Message arrived [" + message.destinationName + "]:\t" + message.payloadString);
 }
 
-
-/*********************************************************
- * OnClick functions
- *********************************************************/
-function switchOnClick(){
-  if (document.getElementById("switchOnOff").checked){
-    // crate new message with payload "on"
-    message = new Paho.MQTT.Message("on");
-
-    // determine topic and QoS for publishing message
-    message.destinationName = topic.BALKON_LED_STATE;
-    message.qos = 2;
-    
-    // publish message
-    client.send(message);
-    console.log("Message published [" + message.destinationName +"]:\t" + message.payloadString);
-  }
-  else {
-    // crate new message with payload "off"
-    message = new Paho.MQTT.Message("off");
-
-    // determine topic and QoS for publishing message
-    message.destinationName = topic.BALKON_LED_STATE;
-    message.qos = 2;
-    
-    // publish message
-    client.send(message);
-    console.log("Message published [" + message.destinationName +"]:\t" + message.payloadString);
-  }
+// called to publish
+function publish(destination, payload){
+  // crate new message with value of range as payload
+  message = new Paho.MQTT.Message(payload);
+  
+  // determine topic and QoS for publishing message
+  message.destinationName = destination;
+  message.qos = 2;
+  
+  // publish message
+  client.send(message);
+  console.log("Message published [" + message.destinationName +"]:\t" + message.payloadString);
 }
 
 
 /*********************************************************
  * OnMessageArrived functions
  *********************************************************/
+
 function ledStateArrived(payload){
   if (payload == "on"){
-    document.getElementById("switchOnOff").checked = true;
+    switchOnOff.checked = true;
   }
   else{
-    document.getElementById("switchOnOff").checked = false;
+    switchOnOff.checked = false;
   }
+}
+
+function ledBrightnessArrived(payload){
+  sliderBrightness.value = payload;
+}
+
+function ledColorArrived(payload){
+  colorPicker.value = payload;
+}
+
+function ledRangeMinArrived(payload){
+  ledRangeMin.value = payload;
+}
+
+function ledRangeMaxArrived(payload){
+  ledRangeMax.value = payload;
+}
+
+/*********************************************************
+ * Initizialistation
+ *********************************************************/
+function initRange1(document){
+  document.value=Math.min(document.value,document.parentNode.childNodes[5].value-1);
+  var value=(100/(parseInt(document.max)-parseInt(document.min)))*parseInt(document.value)-(100/(parseInt(document.max)-parseInt(document.min)))*parseInt(document.min);
+  var children = document.parentNode.childNodes[1].childNodes;
+  children[1].style.width=value+'%';
+  children[5].style.left=value+'%';
+  children[7].style.left=value+'%';children[11].style.left=value+'%';
+  children[11].childNodes[1].innerHTML=document.value;
+}
+
+function initRange2(document){
+  document.value=Math.max(document.value,document.parentNode.childNodes[3].value-(-1));
+  var value=(100/(parseInt(document.max)-parseInt(document.min)))*parseInt(document.value)-(100/(parseInt(document.max)-parseInt(document.min)))*parseInt(document.min);
+  var children = document.parentNode.childNodes[1].childNodes;
+  children[3].style.width=(100-value)+'%';
+  children[5].style.right=(100-value)+'%';
+  children[9].style.left=value+'%';children[13].style.left=value+'%';
+  children[13].childNodes[1].innerHTML=document.value;
+}
+
+function sliderDistanceOnChange(){
+  document.getElementById("inverse-left").style = "width:70%;";
+  document.getElementById("inverse-right").style = "width:70%;";
+
+  document.getElementById("range").style = "left:30%;right:40%;";
+  document.getElementById("thumb1").style = "left:30%;";
+  document.getElementById("thumb2").style = "left:60%;";
+
+  document.getElementById("sign1").style = "left:30%;";
+  document.getElementById("value1") = 30;
+  document.getElementById("sign2").style = "left:60%;";
+  document.getElementById("value2") = 60;
 }
