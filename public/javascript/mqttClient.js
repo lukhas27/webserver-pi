97b705ed8368f197667489fbf1ebca649f7216af
Lukas Haspel <lu391has@htwg-konstanz.de>
2021-04-10 20:49:20 +0200

    update

--- public/javascript/mqttClient.js
+++ public/javascript/mqttClient.js
@@ -1,28 +1,31 @@
 /*********************************************************
  * MQTT Configuration
  *********************************************************/
-MQTT_BROKER = '192.168.0.169';
+MQTT_BROKER = "192.168.0.169";
 MQTT_PORT = 9001;
-MQTT_CLIENTID = 'WebClient' + parseInt(Math.random() * 100);
+MQTT_CLIENTID = "WebClient" + parseInt(Math.random() * 1000);
 
 // definition of topics
-const topic= {
+const topic = {
   BALKON_LEDS_STATE: "balkon/leds/state",
   BALKON_LEDS_COLOR: "balkon/leds/color",
   BALKON_LEDS_BRIGHTNESS: "balkon/leds/brightness",
+  BALKON_LEDS_MODE: "balkon/leds/mode",
   BALKON_LEDS_RANGE_MIN: "balkon/leds/range/min",
   BALKON_LEDS_RANGE_MAX: "balkon/leds/range/max",
-  BALKON_LEDS_STATUS: "balkon/leds/status"
+  BALKON_LEDS_SPEEDFACTOR: "balkon/leds/speedfactor",
+  BALKON_LEDS_STATUS: "balkon/leds/status",
 };
 
 // topic array for subscribing topics
-const subTopics = 
-[
+const subTopics = [
   topic.BALKON_LEDS_STATE,
   topic.BALKON_LEDS_COLOR,
   topic.BALKON_LEDS_BRIGHTNESS,
+  topic.BALKON_LEDS_MODE,
   topic.BALKON_LEDS_RANGE_MIN,
-  topic.BALKON_LEDS_RANGE_MAX
+  topic.BALKON_LEDS_RANGE_MAX,
+  topic.BALKON_LEDS_SPEEDFACTOR,
 ];
 
 // create a client instance
@@ -33,76 +36,95 @@ client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
 
 // connect the client
-client.connect({onSuccess:onConnect});
-
+client.connect({ onSuccess: onConnect });
 
 /*********************************************************
  * HTML Configuration
  *********************************************************/
 
 // switch OnOff
-var switchOnOff = document.getElementById('switch-OnOff');
+var switchOnOff = document.getElementById("switch-OnOff");
 
 // slider brightness
-var sliderBrightness = document.getElementById('slider-brightness');
+var sliderBrightness = document.getElementById("slider-brightness");
+
+// modi buttons
+var button1 = document.getElementById("mode-1");
+var button2 = document.getElementById("mode-2");
+var button3 = document.getElementById("mode-3");
 
 // wheel color picker
-var colorPicker = document.getElementById('color-block');
-
+var colorPicker = document.getElementById("color-block");
 
 // leds range
-var ledRangeMin = document.getElementById('range-min');
-var ledRangeMax = document.getElementById('range-max');
+var setting = {
+  roots: document.querySelector(".my-js-slider"),
+  limits: { minLimit: 0, maxLimit: 29 },
+  rangeValue: { minValue: 0, maxValue: 29 },
+  valueNoteDisplay: true,
+  type: "range",
+  step: 1,
+};
+
+var rangeSlider = wRunner(setting); // inizialies range slider
+
+// init range values
+var ledRangeMin = 0;
+var ledRangeMax = 29;
+var ledRangeMinOld = 0;
+var ledRangeMaxOld = 29;
+
+// speedfactor
+var sliderSpeedfactor = document.getElementById("slider-speedfactor");
 
 /*********************************************************
  * Event Handler
  *********************************************************/
 
 // switch OnOff
-switchOnOff.onchange = function(){
-  if (switchOnOff.checked){
+switchOnOff.onchange = function () {
+  if (switchOnOff.checked) {
     publish(topic.BALKON_LEDS_STATE, "on");
-  }
-  else {
+  } else {
     publish(topic.BALKON_LEDS_STATE, "off");
   }
 };
 
 // slider brightness
-sliderBrightness.oninput = function(){
+sliderBrightness.oninput = function () {
   publish(topic.BALKON_LEDS_BRIGHTNESS, sliderBrightness.value);
 };
 
+// modi buttons
+button1.onclick = function () {
+  publish(topic.BALKON_LEDS_MODE, String(0));
+};
+
+button2.onclick = function () {
+  publish(topic.BALKON_LEDS_MODE, String(1));
+};
+
+button3.onclick = function () {
+  publish(topic.BALKON_LEDS_MODE, String(2));
+};
+
 // color picker
-colorPicker.oncolorchange = function(){
+colorPicker.oncolorchange = function () {
   publish(topic.BALKON_LEDS_COLOR, colorPicker.value);
 };
 
-// led range
-ledRangeMin.oninput = function(){
-  // publish
-  publish(topic.BALKON_LEDS_RANGE_MIN, ledRangeMin.value);
+// 2 point range slider
+rangeSlider.onValueUpdate(function (values) {
+  ledRangeMin = values.minValue;
+  ledRangeMax = values.maxValue;
+  
+  publish(topic.BALKON_LEDS_RANGE_MIN, String(ledRangeMin));
+  publish(topic.BALKON_LEDS_RANGE_MAX, String(ledRangeMax));
+});
 
-  // update slider
-  this.value=Math.min(this.value,this.parentNode.childNodes[5].value-1);
-  var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);
-  var children = this.parentNode.childNodes[1].childNodes;
-  children[1].style.width=value+'%';
-  children[5].style.left=value+'%';
-  children[7].style.left=value+'%';children[11].style.left=value+'%';
-  children[11].childNodes[1].innerHTML=this.value;
-};
-
-ledRangeMax.oninput = function(){
-  publish(topic.BALKON_LEDS_RANGE_MAX, ledRangeMax.value);
-
-  this.value=Math.max(this.value,this.parentNode.childNodes[3].value-(-1));
-  var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);
-  var children = this.parentNode.childNodes[1].childNodes;
-  children[3].style.width=(100-value)+'%';
-  children[5].style.right=(100-value)+'%';
-  children[9].style.left=value+'%';children[13].style.left=value+'%';
-  children[13].childNodes[1].innerHTML=this.value;
+// slider speedfactor
+sliderSpeedfactor.oninput = function () {
+  publish(topic.BALKON_LEDS_SPEEDFACTOR, sliderSpeedfactor.value);
 };
 
 /*********************************************************
@@ -113,7 +135,7 @@ ledRangeMax.oninput = function(){
 function onConnect() {
   console.log("MQTT " + MQTT_CLIENTID + " connected!");
   for (var i = 0; i < subTopics.length; i++) {
-    client.subscribe(subTopics[i], {qos:2});
+    client.subscribe(subTopics[i], { qos: 2 });
     console.log("Subscribed to topic:\t" + subTopics[i]);
   }
 
@@ -124,73 +146,132 @@ function onConnect() {
 // called when the client loses its connection
 function onConnectionLost(responseObject) {
   if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:"+responseObject.errorMessage);
+    console.log("onConnectionLost:" + responseObject.errorMessage);
   }
 }
 
 // called when a message arrives
 function onMessageArrived(message) {
-  switch (message.destinationName){
+  switch (message.destinationName) {
     case topic.BALKON_LEDS_STATE:
-        ledStateArrived(message.payloadString);
+      ledStateArrived(message.payloadString);
       break;
     case topic.BALKON_LEDS_BRIGHTNESS:
-        ledBrightnessArrived(message.payloadString);
+      ledBrightnessArrived(message.payloadString);
+      break;
+    case topic.BALKON_LEDS_MODE:
+      ledModeArrived(message.payloadString);
       break;
     case topic.BALKON_LEDS_COLOR:
-        ledColorArrived(message.payloadString);
+      ledColorArrived(message.payloadString);
       break;
     case topic.BALKON_LEDS_RANGE_MIN:
       ledRangeMinArrived(message.payloadString);
-    break;
+      break;
     case topic.BALKON_LEDS_RANGE_MAX:
       ledRangeMaxArrived(message.payloadString);
-    break;
+      break;
+    case topic.BALKON_LEDS_SPEEDFACTOR:
+      ledSpeedfactorArrived(message.payloadString);
+      break;
   }
-  console.log("Message arrived [" + message.destinationName + "]:\t" + message.payloadString);
+  console.log(
+    "Message arrived [" +
+      message.destinationName +
+      "]:\t" +
+      message.payloadString
+  );
 }
 
 // called to publish
-function publish(destination, payload){
+function publish(destination, payload) {
   // crate new message with value of range as payload
   message = new Paho.MQTT.Message(payload);
-  
+
   // determine topic and QoS for publishing message
   message.destinationName = destination;
   message.qos = 2;
-  
+
   // publish message
   client.send(message);
-  console.log("Message published [" + message.destinationName +"]:\t" + message.payloadString);
+  console.log(
+    "Message published [" +
+      message.destinationName +
+      "]:\t" +
+      message.payloadString
+  );
 }
-
 
 /*********************************************************
  * OnMessageArrived functions
  *********************************************************/
 
-function ledStateArrived(payload){
-  if (payload == "on"){
+function ledStateArrived(payload) {
+  if (payload == "on") {
     switchOnOff.checked = true;
-  }
-  else{
+  } else {
     switchOnOff.checked = false;
   }
 }
 
-function ledBrightnessArrived(payload){
+function ledBrightnessArrived(payload) {
   sliderBrightness.value = payload;
 }
 
-function ledColorArrived(payload){
+function ledModeArrived(payload) {
+  mode = parseInt(payload);
+  switch(mode){
+    case 0:
+      document.getElementById("mode-1").style.opacity = 1;
+      document.getElementById("mode-2").style.opacity = 0.4;
+      document.getElementById("mode-3").style.opacity = 0.4;
+      break;
+    case 1:
+      document.getElementById("mode-1").style.opacity = 0.4;
+      document.getElementById("mode-2").style.opacity = 1;
+      document.getElementById("mode-3").style.opacity = 0.4;
+      break;
+    case 2:
+      document.getElementById("mode-1").style.opacity = 0.4;
+      document.getElementById("mode-2").style.opacity = 0.4;
+      document.getElementById("mode-3").style.opacity = 1;
+      break;
+  }
+}
+
+function ledColorArrived(payload) {
   colorPicker.value = payload;
-  /* colorPickerPB.css("background-color", colorPicker.value); */
+  colorPicker.color = payload;
 }
 
-function ledRangeMinArrived(payload){
-  ledRangeMin.value = payload;
+function ledRangeMinArrived(payload) {
+  /* ledRangeMin = payload;
+
+  if (ledRangeMax - ledRangeMin > 2) {
+    if (ledRangeMin != ledRangeMinOld) {
+      rangeSlider.setRangeValue({
+        minValue: ledRangeMin,
+        maxValue: ledRangeMax,
+      });
+    }
+    ledRangeMinOld = ledRangeMin;
+  } */
 }
 
-function ledRangeMaxArrived(payload){
-  ledRangeMax.value = payload;
+function ledRangeMaxArrived(payload) {
+  /* ledRangeMax = payload;
+  
+  if (ledRangeMax - ledRangeMin > 2) {
+    if (ledRangeMax != ledRangeMaxOld) {
+      rangeSlider.setRangeValue({
+        minValue: ledRangeMin,
+        maxValue: ledRangeMax,
+      });
+    }
+  }
+  ledRangeMaxOld = ledRangeMax; */
+}
+
+function ledSpeedfactorArrived(payload) {
+  sliderSpeedfactor.value = payload;
 }
